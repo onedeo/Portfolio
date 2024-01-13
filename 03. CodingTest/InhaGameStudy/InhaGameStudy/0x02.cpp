5c456a99c528dc04364574b254e1867d42a7b357
iamonedeo <iamonedeo@gmail.com>
2024-01-13 20:35:46 +0900

    asdf

--- 03. CodingTest/InhaGameStudy/InhaGameStudy/0x02.cpp
+++ 03. CodingTest/InhaGameStudy/InhaGameStudy/0x02.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -8,16 +8,33 @@ int main()
 	cin.tie(0);
 	ios::sync_with_stdio(0);
 
+	stack<int> stk;
 	int n, ht;
+	int max;
 	cin >> n;
-	queue<int> que;
-	cin >> ht;
-	que.push(ht);
-	for (int i = 1; i < n; i++)
+	int sum = n - 1;
+	while (n--)
 	{
 		cin >> ht;
-		if(qu)
+		if (stk.empty())
+		{
+			stk.push(ht);
+			max = ht;
+		}
+		else if (max < ht)
+		{
+			sum += stk.size() - 1;
+			while (!stk.empty()) stk.pop();
+			max = ht;
+			stk.push(ht);
+		}
+		else stk.push(ht);
 	}
+	sum += stk.size() - 1;
+	sum--;
+
+	cout << sum;
+
 
 	return 0;
 }
--- 03. CodingTest/InhaGameStudy/InhaGameStudy/0x03.cpp
+++ 03. CodingTest/InhaGameStudy/InhaGameStudy/0x03.cpp
@@ -1,1 +1,23 @@
-
+//#include <iostream>
+//
+//struct Node
+//{
+//	Node* nxt, * prv;
+//	int value;
+//};
+//
+//void Insert(Node node, int addr, int val);
+//void Delete(Node node, int addr);
+//void Search(Node node, int val);
+//
+//void Insert(Node node, int addr, int val)
+//{
+//}
+//
+//void Delete(Node node, int addr)
+//{
+//}
+//
+//void Search(Node node, int val)
+//{
+//}
