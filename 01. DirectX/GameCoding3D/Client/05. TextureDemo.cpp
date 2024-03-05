138cab9186b9ab8e4c6a970101f140f509817497
onedeo <iamonedeo@gmail.com>
2024-03-05 10:57:06 +0900

    qqq

--- 01. DirectX/GameCoding3D/Client/05. TextureDemo.cpp
+++ 01. DirectX/GameCoding3D/Client/05. TextureDemo.cpp
@@ -11,9 +11,9 @@ void TextureDemo::Init()
 
 	// Object
 	_geometry = make_shared<Geometry<VertexTextureData>>();
-	//GeometryHelper::CreateQuad(_geometry);
-	//GeometryHelper::CreateCube(_geometry);
-	//GeometryHelper::CreateSphere(_geometry);
+	GeometryHelper::CreateQuad(_geometry);
+	GeometryHelper::CreateCube(_geometry);
+	GeometryHelper::CreateSphere(_geometry);
 	GeometryHelper::CreateGrid(_geometry, 256, 256);
 
 	_vertexBuffer = make_shared<VertexBuffer>();
